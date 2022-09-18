a39d30160ae2942887be93e4df8aa6573cc16f7c
minh phung <lephungcrazy@gmail.com>
2022-09-18 14:44:43 +0700

    Xay dung trang chi tiet san pham

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'ui/products/products_manager.dart';
+import 'ui/products/product_detail_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -20,9 +23,11 @@ class MyApp extends StatelessWidget {
           secondary: Colors.deepOrange,
         ),
       ),
-      home: Container(
-        color: Colors.green,
+      home: SafeArea(
+        child: ProductDetailScreen(
+          ProductsManager().items[0],
       ),
+    )
     );
   }
 }
